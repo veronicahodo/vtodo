f4783ae69992f2f85998370c6ee84896bb66ded1
Veronica Hodo <90301991+veronicahodo@users.noreply.github.com>
2023-08-08 19:52:56 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,6 +4,13 @@
 
 // Main interface for the vtodo application's server calls
 
+require_once('vcrud.php');
+require_once('vtoken.php');
+require_once('vuser.php');
+
+$crud = new VCRUD($DBUSER,$DBPASS,$DBHOST,$DBNAME);
+
+
 $output = [
     'status'=>'error',
     'message'=>'Unspecified error'
@@ -12,17 +19,26 @@ $output = [
 
 $command = $_REQUEST['command'] ?? '';
 
-switch(strtolower($command) {
-    'createTask':
-       break;
-    'readTask':
-        break;
-    'updateTask':
-        break;
-    'deleteTask':
-        break;
+function doLogin() {
+    // handles the login process.
+    // expects:
+    //    username: the users name. 
+    //        [TODO] make this syntax enforceable
+    //    password: the password in plain text
+
+    $username = $_REQUEST['username'] ?? '';
+    $password = $_REQUEST['password'] ?? '';
+    
 }
 
+switch(strtolower($command) {
+    'login':
+        $output = doLogin();
+       break;
+    default:
+        $output['message'] = 'No valid command specified';
+}
 
+$crud->close();
 header('Content-type: application/json');
 print(json_encode($output));
