804f4e4378d467b641cd52725d35a0fa572aa320
Veronica Hodo <90301991+veronicahodo@users.noreply.github.com>
2023-08-17 00:26:54 -0700

    Update tags.php

--- api/tags.php
+++ api/tags.php
@@ -27,8 +27,42 @@ class Tag {
     }
 
     function read($tagId, VCRUD $c) {
+        // Kids, do not do this at home. This is terrible awful programming
+        // [TODO] Make it not break with a slight breeze. And error check
         return [
             'status'=>'ok',
-            $c->read
+            'tag'=>$c->read($this->table,['tagId','=',$tagId])
+            ];
     }
+
+    function update($fields, VCRUD $c) {
+        // Ok, you know by now that this shit is just unsafe as can be
+        // [TODO] Unfuck.
+        $c->update($this->table,$fields,['tagId','=',$this->fields['tagId']);
+        return ['status'=>'ok'];
+            
+    }
+
+    function delete($tagId, VCRUD $c) {
+        // This should really find would be orphans and move them under
+        // either a catch all or supply? (I don't fucking like that idea)
+        $c->delete($this->table,['tagId','=',$tagId]);
+        return ['status'=>'ok'];
+    }
+
+
+    function search($tags, VCRUD $c) {
+        // searches for all tasks that match ANY of the tags supplied
+        // Once again, coding like a pirate on the carabeean sojsfjslfjsljrf
+        $conditions = [];
+        foreach($tags as $tag) {
+            $conditions[] = ['tagId','
+        }
+        return [
+            'status' => 'ok',
+            'results'=> $results
+            ];
+            
+    }
+
 }
